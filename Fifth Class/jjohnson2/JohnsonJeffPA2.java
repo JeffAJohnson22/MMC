964f2678450e9a70d75e0e8dd057822e86d29f5c
Jeffrey Johnson <jeffrey.johnson@wbd.com>
2025-09-11 21:19:05 -0400

    class five 2 module

--- Fifth Class/jjohnson2/JohnsonJeffPA2.java
+++ Fifth Class/jjohnson2/JohnsonJeffPA2.java
@@ -1,36 +1,43 @@
-// Optimized java implementation of Bubble sort;
-
 /**
- * JohnsonJeffPA2 demonstrates an optimized implementation of Bubble Sort.
- * @author Your Name
- * @version 1.0
+ * Demonstrates an optimized implementation of Bubble Sort.
+ * @author Jeff Johnson
+ * @version 1.0.0
+ * @since Week 2 of CSC6301
  */
 public class JohnsonJeffPA2 {
     /**
      * Sorts an array of integers using the bubble sort algorithm.
      * @param nums the array of integers to sort
      * @return the sorted array
+     * @since Week 2 of CSC6301
      */
-    public static int[] bubbleSortArray(int[] nums) {
-        for(int i=0; i<nums.length; i++){
-            for(int j = 0; j<nums.length-i-1; j++){
-                if(nums[j] > nums[j+1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }               
+    public static int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                System.out.println("Array is sorted");
+                break;
             }
         }
-        return nums;        
+        return array;
     }
 
     /**
      * The main method demonstrates sorting an array using bubbleSortArray.
      * @param args command-line arguments (not used)
+     * @since Week 2 of CSC6301
      */
     public static void main(String[] args) {
         int[] myArray = {2, 45, 37, 21, 31, 50, 29, 22, 67, 88, 56};
-        int[] sortedArray = bubbleSortArray(myArray);
+        int[] sortedArray = bubbleSort(myArray);
         System.out.print("Sorted array: ");
         for (int num : sortedArray) {
             System.out.print(num + " ");
