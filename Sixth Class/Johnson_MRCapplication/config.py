c7ed02b37a7c02ba918d5631ae49bbf3470d2fca
jeffajohnson22 <jeffalanjohnson22@gmail.com>
2025-11-21 15:57:41 -0500

    update we are connectin

---
+++ Sixth Class/Johnson_MRCapplication/config.py
@@ -0,0 +1,9 @@
+# Update these values for your local mysql instance
+
+config = {
+    'username': 'root',
+    'password': 'password',
+    'database': 'mrc',
+    'host': 'localhost',
+    'port': 3306
+}
